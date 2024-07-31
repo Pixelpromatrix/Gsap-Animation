0ced1e77d75a7c1cb1532edcccf5b14737ff6bd7
AceAryanop <as3428925@gmail.com>
2024-07-31 21:18:47 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,61 @@
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
+
+
+var tl = gsap.timeline({scrollTrigger:{
+    trigger:"#mainparentvideo",
+   //  markers:true,
+    start:"50% 50%",
+    end:"150% 50%",
+    scrub:2,
+    pin:true
+}});
+tl
+.to("#videoo",{
+    width: "100%",
+})
+
+
+
+var tl = gsap.timeline({scrollTrigger:{
+    trigger:"#main",
+   //  markers:true,
+    start:"50% 50%",
+    end:"150% 50%",
+    scrub:2,
+    pin:true
+}});
+tl
+.to("#center",{
+   height: "100vh",
+},'a')
+.to("#top",{
+    top: "-50%",
+ },'a')
+ .to("#bottom",{
+    bottom: "-50%",
+ },'a')
+.to("#top-h1",{
+    top: "60%"
+ },'a')
+ .to("#bottom-h1",{
+    bottom: "-30%"
+ },'a')
+.to("#center-h1",{
+   top: "-30%"
+},'a')
+.to(".content",{
+   delay: -0.2,
+   marginTop: "0%"
+})
